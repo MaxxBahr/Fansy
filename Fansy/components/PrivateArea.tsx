93c39d6a72da4d3c86072bd0a31d17acd842a270
MaxxBahr <schnee.bahr@googlemail.com>
2025-07-28 20:53:21 +0200

    added profile pictures

--- Fansy/components/PrivateArea.tsx
+++ Fansy/components/PrivateArea.tsx
@@ -11,17 +11,21 @@ function PrivateArea({ navigation }: { navigation: NavigationProp<any> }) {
         SetContact([
             { name: "John Doe", email: "john.doe@example.com", phone: "123-456-7890" },
             { name: "Jane Smith", email: "jane.smith@example.com", phone: "987-654-3210" },
+            { name: "Hans Meier", email: "hans.meier@example.com", phone: "456-789-0123" },
+            { name: "Peter Müller", email: "peter.mueller@example.com", phone: "321-654-0987" },
+            { name: "Maria Garcia", email: "maria.garcia@example.com", phone: "654-321-0987" },
+            { name: "Sabine", email: "sabine@example.com", phone: "543-210-9876" },
         ])
     }, []);
 
     return(
         <View style={{flex: 1}}>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ flex: 1, paddingTop: 20, paddingHorizontal: 10 }}>
                 {contact.map((c) => (
                     <Contact key={c.name} name={c.name} email={c.email} phone={c.phone} />
                 ))}
             </View>
-            <View style={{ position: 'relative', justifyContent: 'center', alignItems: 'center', elevation: 2, paddingBottom: 20}}>
+            <View style={{ position: 'absolute', bottom: 0, left: 0, right: 0, justifyContent: 'center', alignItems: 'center', elevation: 2, paddingVertical: 20, paddingHorizontal: 10 }}>
                     <AppButton title="Edit NPC" onPress={() => navigation.navigate('EditNPC')} />
             </View>
         </View>
--- Fansy/components/elements/Contacts.tsx
+++ Fansy/components/elements/Contacts.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, Image } from "react-native";
+import AppButton from "./EditButton";
 
 function Contact({name, email, phone}: {name: string, email: string, phone: string}) {
+    // const imageSource = '../images/' + name + '.png';
     return (
-        <View style={{ padding: 10 }}>
-            <Text>Name: {name}</Text>
-            <Text>Email: {email}</Text>
-            <Text>Phone: {phone}</Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center', padding: 20 }}>
+            <Image source={ require('../images/default.png')} style={{ width: 50, height: 50 }}></Image>
+            <View style={{ flex: 1 }}>
+                <Text>{name}</Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                <AppButton title="Call" onPress={() => console.log(`Calling ${name}...`)} />
+                <AppButton title="Message" onPress={() => console.log(`Messaging ${name}...`)} />
+            </View>
         </View>
+    </View>
     );
 }
 export default Contact;
