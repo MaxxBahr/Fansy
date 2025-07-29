d4c507193c29a47251ca6dda92b738a297e4b577
MaxxBahr <schnee.bahr@googlemail.com>
2025-07-29 20:21:08 +0200

    added camera feature

--- Fansy/components/Main.tsx
+++ Fansy/components/Main.tsx
@@ -1,18 +1,68 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, Button } from "react-native";
 import { NavigationProp } from '@react-navigation/native';
 import AppButton from "./elements/EditButton";
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
+
 function Mainpage({ navigation }: { navigation: NavigationProp<any> }) {
-    return(
-        <View style={{flex: 1}}>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Main Page</Text>
-            </View>
-            <View style={{ position: 'relative', justifyContent: 'center', alignItems: 'center', elevation: 2, paddingBottom: 20}}>
-                    <AppButton title="Edit NPC" onPress={() => navigation.navigate('EditNPC')} />
-            </View>
-        </View>
-    )
+  const devices = useCameraDevices();
+  const device = devices.find(device => device.position === 'back');
+
+  const [hasPermission, setHasPermission] = useState(false);
+  const [checkedPermission, setCheckedPermission] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const status = await Camera.getCameraPermissionStatus();
+      console.log("Current status:", status);
+      if (status === 'granted') {
+        setHasPermission(true);
+      }
+      setCheckedPermission(true);
+    })();
+  }, []);
+
+  const handleRequestPermission = async () => {
+    const newStatus = await Camera.requestCameraPermission();
+    console.log("New status:", newStatus);
+    if (newStatus === 'granted' || newStatus === 'denied') {
+      setHasPermission(true);
+    }
+  };
+
+  if (!checkedPermission) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Prüfe Kamera-Erlaubnis...</Text>
+      </View>
+    );
+  }
+
+  if (!hasPermission) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Keine Kamera-Erlaubnis! Bitte erlauben:</Text>
+        <Button title="Kamera-Erlaubnis anfragen" onPress={handleRequestPermission} />
+      </View>
+    );
+  }
+
+  if (!device) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Lade Kamera...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} device={device} isActive={true} />
+      <View style={{ position: 'relative', justifyContent: 'center', alignItems: 'center', elevation: 2, paddingBottom: 20 }}>
+        <AppButton title="Edit NPC" onPress={() => navigation.navigate('EditNPC')} />
+      </View>
+    </View>
+  );
 }
 
 export default Mainpage;
