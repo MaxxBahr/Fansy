839d1567aa477ff73c551f3002d754a8f94eb512
MaxxBahr <schnee.bahr@googlemail.com>
2025-08-18 21:53:21 +0200

    video not working

--- Fansy/components/elements/FloatingImage.tsx
+++ Fansy/components/elements/FloatingImage.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import { useVideoPlayer, VideoView } from 'expo-video';
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
-import Video from "react-native-video";
 
 export default function App() {
   const [videoIndex, setVideoIndex] = useState(0);
@@ -11,22 +11,36 @@ export default function App() {
     require("./assets/video3.mp4"),
   ];
 
+  // Create player with initial video
+  const player = useVideoPlayer(videos[0], (player) => {
+    player.loop = true;
+    player.play();
+  });
+
   const panGesture = Gesture.Pan()
     .onUpdate((event) => {
       if (event.translationY < -100) {
-        setVideoIndex((prev) => prev + 1);
+        setVideoIndex((prev) => (prev + 1) % videos.length);
       }
     });
+
+  // Update player source when videoIndex changes
+  useEffect(() => {
+      console.log('Video index changed to:', videoIndex);
+  console.log('Loading video:', videos[videoIndex]);
+    player.replace(videos[videoIndex]);
+    player.play();
+  }, [videoIndex, player]);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <GestureDetector gesture={panGesture}>
-        <Video
-          source={videos[videoIndex]}
-          style={{ width: "100%", height: "100%" }}
-          resizeMode="cover"
-          shouldPlay
-          isLooping
+        <VideoView
+          player={player}
+          style={styles.video}
+          allowsFullscreen={false}
+          allowsPictureInPicture={false}
+          contentFit="cover"
         />
       </GestureDetector>
     </GestureHandlerRootView>
@@ -40,5 +54,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: { color: "white", fontSize: 24 },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+  text: { 
+    color: "white", 
+    fontSize: 24 
+  },
 });
